53210e0ee7144fe1bee6f2c35e4f1606d87dba4a
PetiKVX <petikvx@gmail.com>
2025-11-07 21:06:06 +0100

    feat: update layout to ensure base URL consistency and add Terms of Service page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://petikvx.github.io',
-  base: '/my-new-site',
+  base: '/my-new-site/',
   integrations: [tailwind()],
   output: 'static',
 });
